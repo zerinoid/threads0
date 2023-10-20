d09ebc6a7b50eeaf4a211daa51ba37239fd1598e
zerinoid <leo.zerino@gmail.com>
2023-10-20 19:59:02 -0300

    feat: copy signed in logic from Topbar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -36,6 +36,21 @@ function LeftSidebar() {
           );
         })}
       </div>
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton>
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
+      </div>
     </section>
   );
 }
