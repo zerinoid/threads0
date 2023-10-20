5652553fbe1f8621a6d325ce23f96c2623988ab7
zerinoid <leo.zerino@gmail.com>
2023-10-20 18:30:52 -0300

    feat: increment Topbar and add image assets

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function Topbar() {
-  return <h1>Topbar</h1>;
+  return (
+    <h1 className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="xt-heading3-bold text-light-1 mx-xs:hidden"> Threads</p>
+      </Link>
+    </h1>
+  );
 }
 
 export default Topbar;
