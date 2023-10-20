9b5d57a474803fa5823994227bdf30bf55ac1409
zerinoid <leo.zerino@gmail.com>
2023-10-20 20:02:53 -0300

    feat: implement signOutCallback to signout button at LeftSidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -38,7 +38,7 @@ function LeftSidebar() {
       </div>
       <div className="mt-10 px-6">
         <SignedIn>
-          <SignOutButton>
+          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
             <div className="flex cursor-pointer gap-4 p-4">
               <Image
                 src="/assets/logout.svg"
