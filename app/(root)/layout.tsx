64724085eea2026f1c9c6c80821be80753a8ddad
zerinoid <leo.zerino@gmail.com>
2023-10-20 19:01:40 -0300

    fix: typo

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           <Topbar />
           <main>
             <LeftSidebar />
-            <section className="main-cointainer">
+            <section className="main-container">
               <div className="w-full max-w-4xl">{children}</div>
             </section>
             <Rightsidebar />
