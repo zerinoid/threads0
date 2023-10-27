5e684cd1cd9ffafb34afd1c59534f7f7de61c94b
zerinoid <leo.zerino@gmail.com>
2023-10-27 02:51:08 -0300

    feat: implement onboarding form with zod and react-hook-form

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,5 +1,21 @@
 "use client";
 
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserValidation } from "@/lib/validation/user";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import * as z from "zod";
+
 interface Props {
   user: {
     id: string;
@@ -13,7 +29,45 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
-  return <div>Account Profile</div>;
+  const form = useForm({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: "",
+      name: "",
+      username: "",
+      bio: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof UserValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default AccountProfile;
