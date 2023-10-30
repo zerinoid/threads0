97353f3e73aab618f02cdb756c1cc01378a865dd
zerinoid <leo.zerino@gmail.com>
2023-10-30 16:19:08 -0300

    feat: mongoose config

---
+++ lib/mongoose.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+let isConnected = false; //Check if mongoose is connected
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
+  if (isConnected) return console.log("Already connect to MongoDB");
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    isConnected = true;
+  } catch (error) {
+    console.log(error, "### MongoDB ###");
+  }
+};
