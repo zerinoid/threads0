0b71d5f19b98030a0d66f28a3bf2bb7daf4a8971
zerinoid <leo.zerino@gmail.com>
2023-11-02 17:05:51 -0300

    feat: implement user threads at home with barebone ThreadCard comp

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,37 @@
+import { ThreadCard } from "@/components/cards/ThreadCard";
 import { fetchThreads } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
 
 export default async function Home() {
   const result = await fetchThreads(1, 30);
+  const user = await currentUser();
 
   console.log(result, "### result  ###");
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {result.threads.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result.threads.map((thread) => (
+              <ThreadCard
+                key={thread._id}
+                id={thread._id}
+                currentUserId={user?.id || ""}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.creadtedAt}
+                comments={thread.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   );
 }
